c4920e2179a78f6fa6d642189eb00f2a13c96fa1
Johannes Antuk <Johannes2306@users.noreply.github.com>
2023-03-06 23:16:36 +0200

    Include JSON prettyfi and base64 encode/decode

--- IoT/MQTT/mqtt_publish.py
+++ IoT/MQTT/mqtt_publish.py
@@ -1,46 +1,40 @@
 #!/usr/bin/env python3
-import argparse
+import argparse, base64, sys
 import paho.mqtt.client as mqtt
-import sys
 
-# Define the callback function for publishing messages
 def on_publish(client, userdata, mid):
     print(f"Message published with ID {mid}")
 
-# Create an argument parser and define the command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("broker_ip", help="MQTT broker IP address")
-parser.add_argument("-p", "--port", type=int, default=1883, help="MQTT broker port (default 1883)")
-parser.add_argument("-t", "--topic", required=True, help="MQTT topic to publish to")
-parser.add_argument("-m", "--message", required=True, help="MQTT message to publish")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("broker_ip", help="MQTT broker IP address")
+    parser.add_argument("-p", "--port", type=int, default=1883, help="MQTT broker port (default 1883)")
+    parser.add_argument("-t", "--topic", required=True, help="MQTT topic to publish to")
+    parser.add_argument("-m", "--message", required=True, help="MQTT message to publish")
+    parser.add_argument("-b", "--base64", action="store_true", help="Encode message with base64")
+    args = parser.parse_args()
 
-# Parse the command-line arguments
-args = parser.parse_args()
+    client = mqtt.Client()
+    client.on_publish = on_publish
 
-# Create an MQTT client instance
-client = mqtt.Client()
+    try:
+        client.connect(args.broker_ip, args.port, 60)
+    except ConnectionRefusedError:
+        print(f"Connection to MQTT broker at {args.broker_ip}:{args.port} refused")
+        sys.exit()
 
-# Set the callback function for publishing messages
-client.on_publish = on_publish
+    message = base64.b64encode(args.message.encode('utf-8')).decode('utf-8') if args.base64 else args.message
+    result, mid = client.publish(args.topic, message)
 
-# Connect to the MQTT broker using the command-line arguments
-try:
-    client.connect(args.broker_ip, args.port, 60)
-except ConnectionRefusedError:
-    print(f"Connection to MQTT broker at {args.broker_ip}:{args.port} refused")
-    sys.exit()
+    if result == mqtt.MQTT_ERR_SUCCESS:
+        print("Waiting for message to be published...")
+        client.loop()
+    else:
+        print(f"Failed to publish message with result code {result}")
+        client.disconnect()
+        sys.exit()
 
-# Publish the message to the specified topic
-result, mid = client.publish(args.topic, args.message)
-
-# Wait for the message to be published before disconnecting from the broker
-if result == mqtt.MQTT_ERR_SUCCESS:
-    print("Waiting for message to be published...")
-    client.loop()
-else:
-    print(f"Failed to publish message with result code {result}")
     client.disconnect()
-    sys.exit()
 
-# Disconnect from the MQTT broker
-client.disconnect()
+if __name__ == "__main__":
+    main()
--- IoT/MQTT/mqtt_subscribe.py
+++ IoT/MQTT/mqtt_subscribe.py
@@ -1,67 +1,84 @@
 #!/usr/bin/env python3
-import argparse
+import argparse, sys, base64, json
 import paho.mqtt.client as mqtt
-import sys
 
-# Define the callback functions for various MQTT events
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT broker")
-        # Automatically subscribe to the specified topics or all topics if none specified
         if args.topics:
             for topic in args.topics:
                 client.subscribe(topic)
         else:
             client.subscribe("#")
     else:
-        print(f"Connection to MQTT broker failed with result code {rc}: {mqtt.connack_string(rc)}")
+        print(
+            f"Connection to MQTT broker failed with result code {rc}: {mqtt.connack_string(rc)}")
         sys.exit()
+
 
 def on_disconnect(client, userdata, rc):
     if rc == mqtt.MQTT_ERR_SUCCESS:
         print("Disconnected from MQTT broker")
     else:
-        print(f"Unexpected disconnection from MQTT broker with result code {rc}: {mqtt.error_string(rc)}")
+        print(
+            f"Unexpected disconnection from MQTT broker with result code {rc}: {mqtt.error_string(rc)}")
         sys.exit()
 
 
 def on_message(client, userdata, message):
-    payload_str = message.payload.decode('utf-8')  # convert bytes to string
-    print(f"Topic:\t{message.topic}\nData:\t{payload_str}\n")
+    payload_str = message.payload.decode('utf-8')
+    if args.base64 and is_base64(payload_str):
+        payload_str = base64.b64decode(payload_str).decode('utf-8')
+    if args.pretty_json and is_json(payload_str):
+        payload_str = json.dumps(json.loads(payload_str), indent=4)
+    print(f"\nTopic:\t{message.topic}\n{payload_str}")
 
 
-# Create an argument parser and define the command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("broker_ip", help="MQTT broker IP address")
-parser.add_argument("-p", "--port", type=int, default=1883,
-                    help="MQTT broker port (default 1883)")
-parser.add_argument("-t", "--ttl", type=int, default=60,
-                    help="MQTT client TTL in seconds (default 60)")
-parser.add_argument("-T", "--topics", nargs="+",
-                    help="MQTT topics to subscribe to (default all topics)")
+def is_base64(s):
+    try:
+        return base64.b64encode(base64.b64decode(s)) == s.encode()
+    except Exception:
+        return False
 
-# Parse the command-line arguments
-args = parser.parse_args()
 
-# Create an MQTT client instance
-client = mqtt.Client()
+def is_json(s):
+    try:
+        json.loads(s)
+        return True
+    except ValueError:
+        return False
 
-# Set the callback functions
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
 
-# Connect to the MQTT broker using the command-line arguments
-try:
-    client.connect(args.broker_ip, args.port, args.ttl)
-except ConnectionRefusedError:
-    print(f"Connection to MQTT broker at {args.broker_ip}:{args.port} refused")
-    sys.exit()
+def main():
+    global args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("broker_ip", help="MQTT broker IP address")
+    parser.add_argument("-p", "--port", type=int, default=1883, help="MQTT broker port (default 1883)")
+    parser.add_argument("-t", "--ttl", type=int, default=60, help="MQTT client TTL in seconds (default 60)")
+    parser.add_argument("-T", "--topics", nargs="+", help="MQTT topics to subscribe to (default all topics)")
+    parser.add_argument("-b", "--base64", action="store_true", help="Try to decode base64 payloads")
+    parser.add_argument("-j", "--pretty-json", action="store_true", help="Try to pretty-print JSON payloads")
+    args = parser.parse_args()
 
-# Start the MQTT client loop to listen for incoming messages
-try:
-    client.loop_forever()
-except KeyboardInterrupt:
-    print("User interrupted")
-finally:
-    client.disconnect()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+
+    try:
+        client.connect(args.broker_ip, args.port, args.ttl)
+    except ConnectionRefusedError:
+        print(f"Connection to MQTT broker at {args.broker_ip}:{args.port} refused")
+        sys.exit()
+
+    try:
+        client.loop_forever()
+    except KeyboardInterrupt:
+        print("User interrupted")
+    finally:
+        client.disconnect()
+
+
+if __name__ == "__main__":
+    main()
